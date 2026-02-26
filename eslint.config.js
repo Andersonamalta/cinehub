e43debfe735b54ae6b0a577a63f4756adce88daf
Anderson <andersonamalta@gmail.com>
2026-02-26 19:15:39 -0300

    add tawild css

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,38 @@
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-config-prettier";
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // 🔥 ESSENCIAL
+        },
+      },
+    },
     plugins: {
       react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
     },
   },
   prettier,
-];
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1>Hello</h1>;
-};
+  return <h1 className="text-blue-500">Hello!!!</h1>
+}
 
-export default App;
+export default App
